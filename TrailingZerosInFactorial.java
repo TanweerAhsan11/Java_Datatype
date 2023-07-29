9bfcb4c10066a0a34143d0f846781dcdead33fc4
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-07-29 20:34:41 +0530

    Finding trailing zeros in any factorial number.

---
+++ TrailingZerosInFactorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TrailingZerosInFactorial {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int trailZeros = 0;
+        int fact=1;
+        for(int i=2;i<=n;i++){
+            fact = fact*i;
+        }
+        while(fact%10 == 0)         // if last digit of factorial is zero we will count and repeat. 
+        {
+            // fact=fact%10;
+            trailZeros++;
+            fact=fact/10;
+        }
+        System.out.println(trailZeros);
+    }
+}
+
+// NOTE :- This will not give appropriate solution for larger value > 12.
