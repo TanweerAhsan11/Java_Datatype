d04b6c4983ab5563bff0a304f066ea379625f873
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-07-30 13:01:13 +0530

    gcd optimized using recursion.

---
+++ gcdOptimized.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class gcdOptimized {
+
+    public static int gcd(int a,int b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        else
+        {
+            return gcd(b,a%b);
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=gcd(a, b);
+        System.out.println(c);
+    }
+}
